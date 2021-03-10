ce97b3d1a31f110f8f182b10b5c419e1b28687e4
Cppshizoid <77567325+cppshizoidS@users.noreply.github.com>
2021-03-10 15:24:06 +0300

    Add files via upload

---
+++ Second lesson.cpp
@@ -0,0 +1,12 @@
+#include <iostream>		//lenth of string
+#include <string>
+using namespace std;
+
+int main()
+{
+	string txt;
+	cout << "Please, enter string:\n";
+	cin >> txt;
+	cout << "The lenth of string is: " << txt.length();
+	return 0;
+}
