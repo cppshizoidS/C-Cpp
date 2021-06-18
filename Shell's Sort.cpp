e9d376e3462fd930d34bec226cbc6ee0f809799c
Cppshizoid <alex.krivoschecov@gmail.com>
2021-06-18 16:42:17 +0300

    Add files via upload

---
+++ Shell's Sort.cpp
@@ -0,0 +1,9 @@
+template< typename RandomAccessIterator, typename Compare > //сортировка Шелла
+void shell_sort( RandomAccessIterator first, RandomAccessIterator last, Compare comp )
+{
+    for( auto d = ( last - first ) / 2; d != 0; d /= 2 )
+//нужен цикл для first = a[0..d-1]
+        for( auto i = first + d; i != last; ++i )
+            for( auto j = i; j - first >= d && comp( *j, *( j - d ) ); j -= d )
+                std::swap( *j, *( j - d ) );
+}
