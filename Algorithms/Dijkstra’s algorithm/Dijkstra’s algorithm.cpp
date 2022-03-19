292543a6c1faf571a9f28b9636c4986196687852
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-19 18:10:20 +0700

    Update Dijkstra’s algorithm.cpp

--- Algorithms/Dijkstra’s algorithm/Dijkstra’s algorithm.cpp
+++ Algorithms/Dijkstra’s algorithm/Dijkstra’s algorithm.cpp
@@ -1,64 +1,67 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-
-int miniDist(int distance[], bool Tset[]) // finding minimum distance
-{
-    int minimum=INT_MAX,ind;
-
-    for(int k=0;k<6;k++)
-    {
-        if(Tset[k]==false && distance[k]<=minimum)
-        {
-            minimum=distance[k];
-            ind=k;
-        }
-    }
-    return ind;
-}
-
-void DijkstraAlgo(int graph[6][6],int src) // adjacency matrix
-{
-    int distance[6]; // // array to calculate the minimum distance for each node
-    bool Tset[6];// boolean array to mark visited and unvisited for each node
-
-
-    for(int k = 0; k<6; k++)
-    {
-        distance[k] = INT_MAX;
-        Tset[k] = false;
-    }
-
-    distance[src] = 0;   // Source vertex distance is set 0
-
-    for(int k = 0; k<6; k++)
-    {
-        int m=miniDist(distance,Tset);
-        Tset[m]=true;
-        for(int k = 0; k<6; k++)
-        {
-            // updating the distance of neighbouring vertex
-            if(!Tset[k] && graph[m][k] && distance[m]!=INT_MAX && distance[m]+graph[m][k]<distance[k])
-                distance[k]=distance[m]+graph[m][k];
-        }
-    }
-    cout<<"Vertex\t\tDistance from source vertex"<<endl;
-    for(int k = 0; k<6; k++)
-    {
-        char str=65+k;
-        cout<<str<<"\t\t\t"<<distance[k]<<endl;
-    }
-}
 
 int main()
 {
-    int graph[6][6]={
-            {0, 1, 2, 0, 0, 0},
-            {1, 0, 0, 5, 1, 0},
-            {2, 0, 0, 2, 3, 0},
-            {0, 5, 2, 0, 2, 2},
-            {0, 1, 3, 2, 0, 1},
-            {0, 0, 0, 2, 1, 0}};
-    DijkstraAlgo(graph,0);
+    system("chcp 1251 > nul");
+
+    vector<int> parent(5, -1);
+    int big_num(10000);
+    int matrix[5][5] = { {0,5,12,0,0},
+                         {5,0,4,0,1},
+                         {12,4,0,2,0},
+                         {0,0,2,0,5},
+                         {0,1,0,5,0} };
+
+    int pos[5], node[5], min(0), index_min(0);
+    for (int i = 0; i < 5; ++i) {
+        pos[i] = big_num;
+        node[i] = 0;
+    }
+    for (int j = 0; j < 5; ++j) {
+        for (int i = 0; i < 5; ++i) {
+            cout << setw(4) << matrix[i][j];
+        }
+        cout << "\n";
+    }
+    int x;
+
+    cout << "Write x1";
+
+    cin >> x;
+    pos[x - 1] = 0; // start pos
+    cout << "\n";
+    for (int i = 0; i < 4; ++i) {
+        min = big_num;
+        for (int j = 0; j < 5; ++j) {
+            if (!node[j] && pos[j] < min) {
+                min = pos[j];
+                index_min = j;
+            }
+        }
+        node[index_min] = true;
+        for (int j = 0; j < 5; ++j) {
+            if (!node[j] && matrix[index_min][j] > 0 && pos[index_min] != big_num && pos[index_min] + matrix[index_min][j] < pos[j]) {
+                pos[j] = pos[index_min] + matrix[index_min][j];
+                parent.at(j) = index_min;
+            }
+        }
+    }
+    int n(0);
+    cout << "\nNumbers of peaks? : ";
+    cin >> n;
+
+    vector<int>temp;
+    for (int i = n - 1; i != -1; i = parent.at(i))temp.push_back(i + 1);
+    reverse(temp.begin(), temp.end());
+    for (int i = 0; i < temp.size(); ++i)cout << temp.at(i) << " ";
+
+    cout << "\nWeight : " << pos[n - 1] << "\n";
+
+    cout << endl;
     return 0;
 }
