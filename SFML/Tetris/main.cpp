59c826ee15da325a73696b1b1d67b33f41c7456b
Cppshizoid <alex.krivoschecov@gmail.com>
2022-03-31 15:20:44 +0700

    Add files via upload

---
+++ SFML/Tetris/main.cpp
@@ -0,0 +1,12 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+const int M = 20;   //height
+const int N = 10;   //width
+
+int manin(){
+    RenderWindow window(VideoMode(320,400), "The Game");
+
+    Texture texture;
+    texture.loadFromFile()
+}
