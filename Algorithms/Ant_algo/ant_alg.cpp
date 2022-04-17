cfe037a17f62c56778d67a172f22c05c7cffe440
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-17 22:56:16 +0300

    Update ant_alg.cpp

--- Algorithms/Ant_algo/ant_alg.cpp
+++ Algorithms/Ant_algo/ant_alg.cpp
@@ -1,1 +1,45 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1000
+#define FRAMERATE 60
+#define CITIES 20
+#define COLOR_DENSITY 20
+#define CONSOLE_OUTPUT 1
+#define ITERATIONS 20
+
+class AntAlgorithm
+{
+    template<typename T> using vector = std::vector<T>;
+
+    struct CityType
+    {
+        int x, y;
+    };
+
+    struct AntType
+    {
+        int currentCity;
+        int nextCity;
+        vector<bool> usedCities;
+        vector<int> path;
+        double pathLength;
+
+        void Restart(size_t newCity, int maxCities)
+        {
+            nextCity = -1;
+            pathLength = 0.0;
+            currentCity = newCity;
+            path.clear();
+            path.push_back(newCity);
+            usedCities.clear();
+            usedCities.resize(maxCities, false);
+            usedCities[newCity] = true;
+        }
+    };
