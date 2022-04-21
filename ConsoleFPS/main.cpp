e58c8b58c10a81d8e364844c49dcb88212181417
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-21 12:15:20 +0300

    Create main.cpp

---
+++ ConsoleFPS/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+
+#include <stdio.h>
+#include <Windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+int nMapWidth = 16;
+int nMapHeight = 16;
+
+float fPlayerX = 14.7f;         //start position
+float fPlayerY = 5.09f;
+float fPlayerA = 0.0f;			// player start rotation
+float fFOV = 3.14159f / 4.0f;	// field of view
+float fDepth = 16.0f;			// maximum rendering distance
+float fSpeed = 5.0f;			// walking Speed
+
+int main()
+{
+    wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    // Create Map of world space # = wall block, . = space
+    wstring map;
+    map += L"#########.......";
+    map += L"#...............";
+    map += L"#.......########";
+    map += L"#..............#";
+    map += L"#......##......#";
+    map += L"#......##......#";
+    map += L"#..............#";
+    map += L"###............#";
+    map += L"##.............#";
+    map += L"#......####..###";
+    map += L"#......#.......#";
+    map += L"#......#.......#";
+    map += L"#..............#";
+    map += L"#......#########";
+    map += L"#..............#";
+    map += L"################";
+
+    auto tp1 = chrono::system_clock::now();
+    auto tp2 = chrono::system_clock::now();
+}
