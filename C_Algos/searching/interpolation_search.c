5ca696db00f7855e3bbada10d4c5778f788ce650
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-28 14:45:32 +0700

    Update interpolation_search.c

--- C_Algos/searching/interpolation_search.c
+++ C_Algos/searching/interpolation_search.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-/* By comparison, binary search always chooses the middle of the remaining
- * search space, discarding one half or the other, depending on the comparison
- * between the key found at the estimated position and the key sought. The
- * remaining search space is reduced to the part before or after the estimated
- * position. The linear search uses equality only as it compares elements
- * one-by-one from the start, ignoring any sorting. On average the interpolation
- * search makes about log(log(n)) comparisons (if the elements are uniformly
- * distributed), where n is the number of elements to be searched. In the worst
- * case (for instance where the numerical values of the keys increase
- * exponentially) it can make up to O(n) comparisons. In
- * interpolation-sequential search, interpolation is used to find an item near
- * the one being searched for, then linear search is used to find the exact
- * item. */
 
 int interpolationSearch(int arr[], int n, int key)
 {
