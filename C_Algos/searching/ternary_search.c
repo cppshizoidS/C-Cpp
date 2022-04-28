661eae0dd4420ff3b1e0c7d2fc10d1077e2b51ef
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-28 14:48:34 +0700

    Update ternary_search.c

--- C_Algos/searching/ternary_search.c
+++ C_Algos/searching/ternary_search.c
@@ -1,7 +1,5 @@
-
 #include <stdio.h>
 
-// Function to perform Ternary Search
 int ternarySearch(int l, int r, int key, int ar[])
 {
     if (r >= l)
@@ -20,24 +18,16 @@ int ternarySearch(int l, int r, int key, int ar[])
             return mid2;
         }
 
-        // Since key is not present at mid,
-        // check in which region it is present
-        // then repeat the Search operation
-        // in that region
-
         if (key < ar[mid1])
         {
-            // The key lies in between l and mid1
             return ternarySearch(l, mid1 - 1, key, ar);
         }
         else if (key > ar[mid2])
         {
-            // The key lies in between mid2 and r
             return ternarySearch(mid2 + 1, r, key, ar);
         }
         else
         {
-            // The key lies in between mid1 and mid2
             return ternarySearch(mid1 + 1, mid2 - 1, key, ar);
         }
     }
@@ -46,40 +36,22 @@ int ternarySearch(int l, int r, int key, int ar[])
     return -1;
 }
 
-// Driver code
 int main()
 {
     int l, r, p, key;
-
-    // Get the array
-    // Sort the array if not sorted
     int ar[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     // Starting index
     l = 0;
-
     // length of array
     r = 9;
 
-    // Checking for 5
 
-    // Key to be searched in the array
     key = 5;
-
-    // Search the key using ternarySearch
     p = ternarySearch(l, r, key, ar);
-
-    // Print the result
     printf("Index of %d is %d\n", key, p);
-
-    // Checking for 50
-
-    // Key to be searched in the array
+    
     key = 50;
-
-    // Search the key using ternarySearch
     p = ternarySearch(l, r, key, ar);
-
-    // Print the result
     printf("Index of %d is %d", key, p);
 }
