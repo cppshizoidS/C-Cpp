6a8828c26df8976b4193dc5bc0f635039ddc2851
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-28 14:44:43 +0700

    Update fibonacci_search.c

--- C_Algos/searching/fibonacci_search.c
+++ C_Algos/searching/fibonacci_search.c
@@ -8,8 +8,7 @@ int fibMonaccianSearch(int arr[], int x, int n)
     int fibMMm1 = 1;               // (m-1)'th Fibonacci No.
     int fibM = fibMMm2 + fibMMm1;  // m'th Fibonacci
 
-    /* fibM is going to store the smallest Fibonacci
-       Number greater than or equal to n */
+
     while (fibM < n)
     {
         fibMMm2 = fibMMm1;
@@ -20,18 +19,11 @@ int fibMonaccianSearch(int arr[], int x, int n)
     // Marks the eliminated range from front
     int offset = -1;
 
-    /* while there are elements to be inspected. Note that
-       we compare arr[fibMm2] with x. When fibM becomes 1,
-       fibMm2 becomes 0 */
+ 
     while (fibM > 1)
     {
-        // Check if fibMm2 is a valid location
-
-        // sets i to the min. of (offset+fibMMm2) and (n-1)
         int i = ((offset + fibMMm2) < (n - 1)) ? (offset + fibMMm2) : (n - 1);
 
-        /* If x is greater than the value at index fibMm2,
-           cut the subarray array from offset to i */
         if (arr[i] < x)
         {
             fibM = fibMMm1;
@@ -40,8 +32,7 @@ int fibMonaccianSearch(int arr[], int x, int n)
             offset = i;
         }
 
-        /* If x is greater than the value at index fibMm2,
-           cut the subarray after i+1  */
+       
         else if (arr[i] > x)
         {
             fibM = fibMMm2;
@@ -49,16 +40,13 @@ int fibMonaccianSearch(int arr[], int x, int n)
             fibMMm2 = fibM - fibMMm1;
         }
 
-        /* element found. return index */
         else
             return i;
     }
 
-    /* comparing the last element with x */
     if (fibMMm1 && arr[offset + 1] == x)
         return offset + 1;
 
-    /*element not found. return -1 */
     return -1;
 }
 
