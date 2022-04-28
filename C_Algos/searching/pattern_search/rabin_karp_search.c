c198ebdf207a852b1a6d974943d49da5a7a1e71c
Cppshizoid <alex.krivoschecov@gmail.com>
2022-04-28 14:49:23 +0700

    Update rabin_karp_search.c

--- C_Algos/searching/pattern_search/rabin_karp_search.c
+++ C_Algos/searching/pattern_search/rabin_karp_search.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Kabin-Karp algorithm for pattern searching
-   d: radix-d notation. Ex. number from 0->9, d = 10
-   q: prime number for hashing */
+
 void rabin_karp_search(char *str, char *pattern, int d, int q)
 {
     int len_str = strlen(str);
@@ -14,7 +12,6 @@ void rabin_karp_search(char *str, char *pattern, int d, int q)
 
     /* h = pow(d, len_pat - 1) % q */
     for (i = 0; i < len_pat - 1; i++) h = d * h % q;
-    /* Calculating hashing of pattern and the 1st window of text */
     for (i = 0; i < len_pat; i++)
     {
         hash_p = (d * hash_p + pattern[i]) % q;
@@ -23,9 +20,7 @@ void rabin_karp_search(char *str, char *pattern, int d, int q)
 
     for (i = 0; i <= len_str - len_pat; i++)
     {
-        /* Check hash value of current window of text, and pattern
-           If it is match, check each character to make sure pattern
-           is match with current window of text */
+        
         if (hash_p == hash_s)
         {
             int j;
@@ -37,10 +32,7 @@ void rabin_karp_search(char *str, char *pattern, int d, int q)
             if (len_pat == j)
                 printf("--Pattern is found at: %d\n", i);
         }
-        /* Calculate hash value for next window by removing the leading
-           element of current window text, and adding its trailing */
         hash_s = (d * (hash_s - str[i] * h) + str[i + len_pat]) % q;
-        /* Converting hash value to positive when it is negative */
         if (hash_s < 0)
             hash_s = hash_s + q;
     }
