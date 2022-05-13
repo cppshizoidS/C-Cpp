a34c7d79d06b64d330f0156ae1eea775a1c3d0c9
Cppshizoid <alex.krivoschecov@gmail.com>
2022-05-13 08:44:28 +0700

    Update noise.c

--- Qt_5/Sea_Battle/src/Client/noise.c
+++ Qt_5/Sea_Battle/src/Client/noise.c
@@ -1,30 +1,3 @@
-/* ============================================================================
-**
-** Perlin noise
-** Copyright (C) 2005  Julien Guertault
-**
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the GNU General Public License
-** as published by the Free Software Foundation; either version 2
-** of the License, or (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-**
-** ========================================================================= */
-
-/*
-** Improved Perlin noise.
-** Original Perlin noise implementation can be found at :
-** http://mrl.nyu.edu/~perlin/doc/oscar.html#noise
-*/
-
 #include <stdlib.h>
 
 #define		MOD	0xff
