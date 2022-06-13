0d6befbaaa8b82e59be668d167be98fd3916541f
Cppshizoid <alex.krivoschecov@gmail.com>
2022-06-14 00:58:07 +0700

    Create GLCoreUtils.h

---
+++ OpenGL/Paticles/src/GLCoreUtils.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Utility header file 
+
+#include "GLCore/Util/Shader.h"
+#include "GLCore/Util/OrthographicCamera.h"
+#include "GLCore/Util/OrthographicCameraController.h"
+#include "GLCore/Util/OpenGLDebug.h"
